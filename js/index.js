9eb5581f2743a151d2135dc7f266d12e8aefab25
Martin Podloucký <podlouckymartin@gmail.com>
2019-05-23 09:54:45 +0200

    Solution to the step 1 of the morning workout. Pacman opens and closes his mouth on document click.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 const TILE_SIZE = 85;
+let pacMouth = 'closed';
 
 document.addEventListener('DOMContentLoaded', () => {
   const pac = document.querySelector('.pac');
   pac.style.backgroundImage = 'url(img/pacboy-active-light.png)';
   pac.style.backgroundPositionX = TILE_SIZE + 'px';
+
+  document.addEventListener('click', () => {
+    if(pacMouth === 'closed') {
+      pac.style.backgroundPositionX = '0px';
+      pacMouth = 'opened';
+    } else {
+      pac.style.backgroundPositionX = TILE_SIZE + 'px';
+      pacMouth = 'closed';
+    }
+  });
 });
