648eb3115c698cb33cc4497014cc9badc66f1159
Martin Podloucký <podlouckymartin@gmail.com>
2019-05-23 11:27:46 +0200

    Solution to stage 2 - pacman as a class.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,43 @@
 const TILE_SIZE = 85;
-let pacMouth = 'closed';
-let pacX = 0;
+
+class Pacman {
+  constructor(pacX) {
+    this.pacX = pacX;
+    this.pacMouth = 'closed'
+  }
+
+  move() {
+    this.pacX += 1;
+
+    if(this.pacMouth === 'closed') {
+      this.pacMouth = 'opened';
+    } else {
+      this.pacMouth = 'closed';
+    }
+
+    this.update();
+  }
+
+  update() {
+    const pac = document.querySelector('.pac');
+    pac.style.backgroundImage = 'url(img/pacboy-active-light.png)';
+    pac.style.left = this.pacX * TILE_SIZE + 'px';
+
+    if(this.pacMouth === 'opened') {
+      pac.style.backgroundPositionX = '0px';
+    } else {
+      pac.style.backgroundPositionX = TILE_SIZE + 'px';
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const pac = document.querySelector('.pac');
-  pac.style.backgroundImage = 'url(img/pacboy-active-light.png)';
-  pac.style.backgroundPositionX = TILE_SIZE + 'px';
+  const robert = new Pacman(3);
+  robert.update();
 
   document.addEventListener('keydown', (event) => {
     if(event.code === 'ArrowRight') {
-      pacX += 1;
-      pac.style.left = pacX * TILE_SIZE + 'px';
-      
-      if(pacMouth === 'closed') {
-        pac.style.backgroundPositionX = '0px';
-        pacMouth = 'opened';
-      } else {
-        pac.style.backgroundPositionX = TILE_SIZE + 'px';
-        pacMouth = 'closed';
-      }
+      robert.move();
     }
   });
 });
