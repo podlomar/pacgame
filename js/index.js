233621f3c61fca885b2377ba855fbcbed8787461
Martin Podloucký <podlouckymartin@gmail.com>
2019-05-23 15:43:00 +0200

    Solution to stage 5 - pacman moving in all directions.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
-  const stage = new Stage(6, 3);
+  const stage = new Stage(14, 6);
   stage.mount(app);
   
-  const robert = new Pacman(3);
+  const robert = new Pacman(stage, 3, 1);
   robert.mount(stage.element);
 
   document.addEventListener('keydown', (event) => {
-    if(event.code === 'ArrowRight') {
-      robert.move();
+    if(event.code.startsWith('Arrow')) {
+      robert.move(event.code.substr(5).toLowerCase());
     }
   });
 });
--- js/pacman.js
+++ js/pacman.js
@@ -1,12 +1,37 @@
 class Pacman {
-  constructor(pacX) {
+  constructor(stage, pacX, pacY) {
+    this.stage = stage;
     this.pacX = pacX;
-    this.pacMouth = 'closed'
+    this.pacY = pacY;
+    this.pacMouth = 'closed';
+    this.facing = 'right';
   }
 
-  move() {
-    this.pacX += 1;
-
+  move(dir) {
+    this.facing = dir;
+    switch(dir) {
+      case 'right':
+        if(this.pacX < this.stage.width - 1) {
+          this.pacX += 1;
+        }
+        break;
+      case 'left':
+        if(this.pacX > 0) {
+          this.pacX -= 1;
+        }
+        break;
+      case 'up':
+        if(this.pacY > 0) {
+          this.pacY -= 1;
+        }
+        break;
+      case 'down':
+        if(this.pacY < this.stage.height - 1) {
+          this.pacY += 1;
+        }
+        break;
+    }
+    
     if(this.pacMouth === 'closed') {
       this.pacMouth = 'opened';
     } else {
@@ -26,11 +51,27 @@ class Pacman {
   update() {
     this.element.style.backgroundImage = 'url(img/pacboy-active-light.png)';
     this.element.style.left = this.pacX * TILE_SIZE + 'px';
+    this.element.style.top = this.pacY * TILE_SIZE + 'px';
 
     if(this.pacMouth === 'opened') {
       this.element.style.backgroundPositionX = '0px';
     } else {
       this.element.style.backgroundPositionX = TILE_SIZE + 'px';
+    }
+
+    switch(this.facing) {
+      case 'right':
+        this.element.style.backgroundPositionY = '0px';
+        break;
+      case 'up':
+        this.element.style.backgroundPositionY = TILE_SIZE + 'px';
+        break;
+      case 'down':
+        this.element.style.backgroundPositionY = 2*TILE_SIZE + 'px';
+        break;
+      case 'left':
+        this.element.style.backgroundPositionY = 3*TILE_SIZE + 'px';
+        break;
     }
   }
 
